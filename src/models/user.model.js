efec1268705dc7bbd6f137354a5f14abed5f19a0
Tamannadk <tamannadk4@gmail.com>
2024-10-28 13:52:00 +0530

    register and login controllers

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -46,4 +46,43 @@ const userSchema = new Schema({
   },
 });
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+
+
+userSchema.methods.generateAccessToken = function () {
+  const payload = {
+    _id: this._id,
+    email: this.email,
+    username: this.username,
+    fullName: this.fullName,
+  };
+  const secret_key = process.env.ACCESS_TOKEN_SECRET;
+  const options = {
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+  };
+
+  return jwt.sign(payload, secret_key, options);
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  const payload = {
+    _id: this._id,
+  };
+  const secret_key = process.env.REFRESH_TOKEN_SECRET;
+  const options = {
+    expiresIn: process.env.REFRESH_TOKEN_SECRE,
+  };
+  return jwt.sign(payload, secret_key, options);
+};
 export const User = mongoose.model("User", userSchema);
